d4cf74d5050cd00d1564e6649f8038ffef5098df
Surya-Premkumar <Surya.premkumar@city.ac.uk>
2024-10-22 16:11:48 +0100

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -7,8 +7,9 @@ VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
 INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
 VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
 
-INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
-VALUES ('Fluffy', '1995-05-15', 'litter', '5 kittens, 2 male, 3 female');
+UPDATE petEvent
+SET remark = '5 kittens, 2 male, 3 female'
+WHERE petname = 'Fluffy', eventdate = '1995-05-15'
 
 INSERT INTO petEvent (petname, eventdate, eventtype, remark) 
 VALUES ('Claws', '1997-08-03', 'vet', 'broken rib');
